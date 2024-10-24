c920e4e2887b219b0c25b53495bedc4b8229c83f
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2024-10-24 12:27:10 -0400

    refactor: add user creation and authentication endpoints

--- service/rest-api/internal/routes/routes.go
+++ service/rest-api/internal/routes/routes.go
@@ -4,6 +4,7 @@ import (
 	adapter "service/rest-api/internal/adapter/http"
 	"service/rest-api/internal/adapter/repository"
 	"service/rest-api/internal/core/application/service"
+
 	"github.com/jackc/pgx/v5"
 	"github.com/labstack/echo/v4"
 )
@@ -19,4 +20,12 @@ func RegisterRoutes(e *echo.Echo, conn *pgx.Conn) {
 
 	e.GET("/client", clientHandler.GetAllClients)
 	e.GET("/client/:id", clientHandler.GetClientById)
+	e.POST("/user", clientHandler.CreateUser)
+
+	cognitoRepo := repository.NewCognitoRepository()
+	authService := service.NewAuthService(cognitoRepo)
+	authHandler := adapter.NewAuthHandler(authService)
+
+	e.POST("/auth/signup", authHandler.SignUp)
+	e.POST("/auth/confirm", authHandler.ConfirmAccount)
 }
